95025c92ca0d2335b5a2864cb6cc73a3d01297cc
LordMuetze <valentin-herrmann@gmx.net>
2017-12-13 19:10:31 +0100

    Zahleneingabe mit Kontrolle fuer nat. Zahlen

--- main.py
+++ main.py
@@ -9,35 +9,52 @@ def startseite():
 
 
 def rechenart():
-    
-    msg = "Welche Rechenarten möchtest du üben?"
+
+    msg = "Welche Rechenarten willst du trainieren?"
     title = "Rechenart"
     choices = ["Addition","Multiplikation","Addition & Multiplikation"]
 
     rechenart = buttonbox(msg, title, choices)
-    
+
     return rechenart
 
 
 
 def zahlenraum():
-    
-    msg = "In welchem Bereich soll die erste Zahl liegen?"
-    title = "Zahlenraum"
-    fields = ["kleinste Zahl","größte Zahl"]
-    values = [1,100]
 
-    zahlenraum_1 = multenterbox(msg,title,fields,values)
-    zahl1_max = zahlenraum_1.pop()
-    zahl1_min = zahlenraum_1.pop()
+    while True:
+
+        try:
+            msg = "In welchem Bereich soll die erste Zahl liegen?"
+            title = "Zahlenraum"
+            fields = ["min. Zahl","max. Zahl"]
+            values = [1,100]
+
+            zahlenraum_1 = multenterbox(msg,title,fields,values)
+
+            zahl1_max = int(zahlenraum_1.pop())
+            zahl1_min = int(zahlenraum_1.pop())
+            break
+
+        except ValueError:
+            msgbox("Sorry, das war keine natuerliche Zahl")
+
+
+        msg = "Sorry, das war keine natuerliche Zahl"
+        title = "Zahlenraum"
+        fields = ["min. Zahl","max. Zahl"]
+        values = [1,100]
+
+        zahlenraum_1 = multenterbox(msg,title,fields,values)
+        zahl1_max = zahlenraum_1.pop()
+        zahl1_min = zahlenraum_1.pop()
 
 
     msg = "In welchem Bereich soll die zweite Zahl liegen?"
     title = "Zahlenraum"
-    fields = ["kleinste Zahl","größte Zahl"]
+    fields = ["min.","max. Zahl"]
     values = [1,100]
 
     zahlenraum_2 = multenterbox(msg,title,fields,values)
     zahl2_max = zahlenraum_2.pop()
     zahl2_min = zahlenraum_2.pop()
-
