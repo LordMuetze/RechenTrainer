f58772aab0d63f30426218c362a36d9f93a97df0
LordMuetze <valentin-herrmann@gmx.net>
2017-12-14 16:39:56 +0100

    Kommentarstruktur erstellt

--- main.py
+++ main.py
@@ -1,25 +1,67 @@
+#Kommentar-Vorlage
+#--------------------_____--------------------
+#----------------------------------------------------------------------------------------------------
+
+
+
+#--------------------Modulimport--------------------
 from easygui import *
 from time import *
+#----------------------------------------------------------------------------------------------------
 
 
 
+
+#--------------------Definition der Startseite--------------------
 def startseite():
     pass
+#----------------------------------------------------------------------------------------------------
 
 
 
+
+#--------------------Definition, welche Rechenart trainiert werden soll--------------------
 def rechenart():
 
     msg = "Welche Rechenarten willst du trainieren?"
     title = "Rechenart"
-    choices = ["Addition","Multiplikation","Addition & Multiplikation"]
+    choices = ["Addition","Subtraktion","Multiplikation","Division","verschiedene"]
 
     rechenart = buttonbox(msg, title, choices)
 
     return rechenart
+#----------------------------------------------------------------------------------------------------
 
 
 
+
+#--------------------Definition der einzelnen Aufgabenstellung, rechenart-spezifisch, inhaltlich gleich--------------------
+def Addition(nummer1,nummer2,min_zahl,max_zahl):
+    
+
+
+    enterbox(msg,title)
+#----------------------------------------------------------------------------------------------------
+
+#----------------------------------------------------------------------------------------------------
+def Subtraktion(nummer1,nummer2,min_zahl,max_zahl):
+    pass
+#----------------------------------------------------------------------------------------------------
+
+#----------------------------------------------------------------------------------------------------
+def Multiplikation(nummer1,nummer2,min_zahl,max_zahl):
+    pass
+#----------------------------------------------------------------------------------------------------
+
+#----------------------------------------------------------------------------------------------------
+def Division(nummer1,nummer2,min_zahl,max_zahl):
+    pass
+#----------------------------------------------------------------------------------------------------
+
+
+
+
+#--------------------Festlegung des Zahlenraums--------------------
 def zahlenraum():
 
     while True:
@@ -65,7 +107,7 @@ def zahlenraum():
 
             except ValueError:
                 msgbox("Sorry, da war was bei deiner Eingabe falsch")
-
+#----------------------------------------------------------------------------------------------------
 
 
 
