06c98e86bd0e61a571d84cda103892ed9d2c19c1
Lord Muetze <valentin-herrmann@gmx.net>
2017-12-15 16:39:25 +0100

    Rechendefinitionen komplett

--- main.py
+++ main.py
@@ -7,6 +7,7 @@
 
 #--------------------Modulimport--------------------
 from easygui import *
+import easygui
 from time import *
 from random import *
 #----------------------------------------------------------------------------------------------------
@@ -16,6 +17,8 @@ from random import *
 
 #--------------------erstellen von Variablen--------------------
 richtige_loesungen = 0
+aufgabenliste = []
+nutzer_loesungsliste = []
 #----------------------------------------------------------------------------------------------------
 
 
@@ -23,7 +26,7 @@ richtige_loesungen = 0
 
 #--------------------Definition der Startseite--------------------
 def startseite():
-    pass
+    msgbox("Hallo")
 #----------------------------------------------------------------------------------------------------
 
 
@@ -56,6 +59,8 @@ def anzahl_aufgaben():
 def Addition(min_zahl,max_zahl):
     
     global richtige_loesungen
+    global aufgabenliste
+    global nutzer_loesungsliste
 
     richtige_loesungen += 1
 
@@ -63,40 +68,62 @@ def Addition(min_zahl,max_zahl):
     nummer_2 = randint(min_zahl, max_zahl)
     ergebnis = nummer_1 + nummer_2
 
-    for i in range(0,2):
-        
+    aufgabenstellung = str(nummer_1) + " + " + str(nummer_2) + " = "
+    aufgabenloesung = aufgabenstellung + str(ergebnis)
+    title = "Addition"
 
-        msg = str(nummer_1) + " + " + str(nummer_2) + " = "
-        title = "Addition"
+    i = 0
 
+    while i < 2:        
 
         while True:
             
             try:
-                eingabe = int(enterbox(msg,title))
-                break
+                eingabe = enterbox(aufgabenstellung,title)
+
+                if eingabe == None:
+                    break
+
+                else:
+                    eingabe_korrigiert = int(eingabe)
+                    break
             
             except ValueError:
-                msgbox("Sorry da was bei deiner Eingabe falsch (z.B. falsche Zahlenart)")        
+                msgbox("Sorry da was bei deiner Eingabe falsch\n(z.B. falsche Zahlenart oder Komma statt Punkt bei Kommazahlen)")        
         
         
-        if eingabe == ergebnis:
-            msgbox("Sehr gut, das war richtig!")
-        
+        if eingabe == None:
+            break
+
         else:
+            if eingabe_korrigiert == ergebnis:
+                msgbox("Sehr gut, das war richtig!")
+                i += 2
             
-            if i == 0:
-                msgbox("Schade, das war falsch. Probiere es noch einmal.\nWenn du es dieses mal richtig machst, gibt es einen halben Punkt.")
-                richtige_loesungen -= 0.5
-                
             else:
-                msgbox("Schade, das war falsch.\nDas gibt leider keine Punkte.")
-                richtige_loesungen -= 0.5
+                
+                if i == 0:
+                    msgbox("Schade, das war falsch. Probiere es noch einmal.\nWenn du es dieses mal richtig machst, gibt es einen halben Punkt.")
+                    richtige_loesungen -= 0.5
+                    i += 1
+
+                else:
+                    msgbox("Schade, das war falsch.\nDas gibt leider keine Punkte.")
+                    richtige_loesungen -= 0.5
+                    i += 1
+    
+    if eingabe == None:
+        startseite()
+    else:
+        aufgabenliste.append(aufgabenloesung)
+        nutzer_loesungsliste.append(eingabe_korrigiert)
 #----------------------------------------------------------------------------------------------------
 
 #----------------------------------------------------------------------------------------------------
 def Subtraktion(min_zahl,max_zahl):
     global richtige_loesungen
+    global aufgabenliste
+    global nutzer_loesungsliste
 
     richtige_loesungen += 1
 
@@ -104,117 +131,181 @@ def Subtraktion(min_zahl,max_zahl):
     nummer_2 = randint(min_zahl, max_zahl)
     ergebnis = nummer_1 - nummer_2
 
-    for i in range(0,2):
-        
+    aufgabenstellung = str(nummer_1) + " - " + str(nummer_2) + " = "
+    aufgabenloesung = aufgabenstellung + str(ergebnis)
+    title = "Subtraktion"
 
-        msg = str(nummer_1) + " - " + str(nummer_2) + " = "
-        title = "Subtraktion"
+    i = 0
 
+    while i < 2:        
 
         while True:
             
             try:
-                eingabe = int(enterbox(msg,title))
-                break
+                eingabe = enterbox(aufgabenstellung,title)
+
+                if eingabe == None:
+                    break
+
+                else:
+                    eingabe_korrigiert = int(eingabe)
+                    break
             
             except ValueError:
-                msgbox("Sorry da was bei deiner Eingabe falsch (z.B. falsche Zahlenart)")        
+                msgbox("Sorry da was bei deiner Eingabe falsch\n(z.B. falsche Zahlenart oder Komma statt Punkt bei Kommazahlen)")        
         
         
-        if eingabe == ergebnis:
-            msgbox("Sehr gut, das war richtig!")
+        if eingabe == None:
+            break
         
         else:
+            if eingabe_korrigiert == ergebnis:
+                msgbox("Sehr gut, das war richtig!")
+                i += 2
             
-            if i == 0:
-                msgbox("Schade, das war falsch. Probiere es noch einmal.\nWenn du es dieses mal richtig machst, gibt es einen halben Punkt.")
-                richtige_loesungen -= 0.5
-                
             else:
-                msgbox("Schade, das war falsch.\nDas gibt leider keine Punkte.")
-                richtige_loesungen -= 0.5
+                
+                if i == 0:
+                    msgbox("Schade, das war falsch. Probiere es noch einmal.\nWenn du es dieses mal richtig machst, gibt es einen halben Punkt.")
+                    richtige_loesungen -= 0.5
+                    i += 1
+
+                else:
+                    msgbox("Schade, das war falsch.\nDas gibt leider keine Punkte.")
+                    richtige_loesungen -= 0.5
+                    i += 1
+    
+    if eingabe == None:
+        startseite()
+    else:
+        aufgabenliste.append(aufgabenloesung)
+        nutzer_loesungsliste.append(eingabe_korrigiert)
 #----------------------------------------------------------------------------------------------------
 
 #----------------------------------------------------------------------------------------------------
 def Multiplikation(min_zahl,max_zahl):
     global richtige_loesungen
-
+    global aufgabenliste
+    global nutzer_loesungsliste
+    
     richtige_loesungen += 1
 
     nummer_1 = randint(min_zahl, max_zahl)
     nummer_2 = randint(min_zahl, max_zahl)
     ergebnis = nummer_1 * nummer_2
 
-    for i in range(0,2):
-        
+    aufgabenstellung = str(nummer_1) + " x " + str(nummer_2) + " = "
+    aufgabenloesung = aufgabenstellung + str(ergebnis)
+    title = "Multiplikation"
 
-        msg = str(nummer_1) + " x " + str(nummer_2) + " = "
-        title = "Multiplikation"
+    i = 0
 
+    while i < 2:        
 
         while True:
             
             try:
-                eingabe = int(enterbox(msg,title))
-                break
+                eingabe = enterbox(aufgabenstellung,title)
+
+                if eingabe == None:
+                    break
+
+                else:
+                    eingabe_korrigiert = int(eingabe)
+                    break
             
             except ValueError:
-                msgbox("Sorry da was bei deiner Eingabe falsch (z.B. falsche Zahlenart)")        
+                msgbox("Sorry da was bei deiner Eingabe falsch\n(z.B. falsche Zahlenart oder Komma statt Punkt bei Kommazahlen)")        
         
         
-        if eingabe == ergebnis:
-            msgbox("Sehr gut, das war richtig!")
+        if eingabe == None:
+            break
         
         else:
+            if eingabe_korrigiert == ergebnis:
+                msgbox("Sehr gut, das war richtig!")
+                i += 2
             
-            if i == 0:
-                msgbox("Schade, das war falsch. Probiere es noch einmal.\nWenn du es dieses mal richtig machst, gibt es einen halben Punkt.")
-                richtige_loesungen -= 0.5
-                
             else:
-                msgbox("Schade, das war falsch.\nDas gibt leider keine Punkte.")
-                richtige_loesungen -= 0.5
+                
+                if i == 0:
+                    msgbox("Schade, das war falsch. Probiere es noch einmal.\nWenn du es dieses mal richtig machst, gibt es einen halben Punkt.")
+                    richtige_loesungen -= 0.5
+                    i += 1
+
+                else:
+                    msgbox("Schade, das war falsch.\nDas gibt leider keine Punkte.")
+                    richtige_loesungen -= 0.5
+                    i += 1
+    
+    if eingabe == None:
+        startseite()
+    else:
+        aufgabenliste.append(aufgabenloesung)
+        nutzer_loesungsliste.append(eingabe_korrigiert)
 #----------------------------------------------------------------------------------------------------
 
 #----------------------------------------------------------------------------------------------------
 def Division(min_zahl,max_zahl):
     global richtige_loesungen
-
+    global aufgabenliste
+    global nutzer_loesungsliste
+    
     richtige_loesungen += 1
 
     nummer_1 = randint(min_zahl, max_zahl)
     nummer_2 = randint(min_zahl, max_zahl)
     ergebnis = nummer_1 / nummer_2
 
-    for i in range(0,2):
-        
+    aufgabenstellung = str(nummer_1) + " : " + str(nummer_2) + " = "
+    aufgabenloesung = aufgabenstellung + str(ergebnis)
+    title = "Division"
 
-        msg = str(nummer_1) + " : " + str(nummer_2) + " = "
-        title = "Division"
+    i = 0
 
+    while i < 2:        
 
         while True:
             
             try:
-                eingabe = int(enterbox(msg,title))
-                break
+                eingabe = enterbox(aufgabenstellung+"\n[zwei Nachkommastellen]",title)
+
+                if eingabe == None:
+                    break
+
+                else:
+                    eingabe_korrigiert = float(eingabe)
+                    break
             
             except ValueError:
-                msgbox("Sorry da was bei deiner Eingabe falsch (z.B. falsche Zahlenart)")        
+                msgbox("Sorry da was bei deiner Eingabe falsch\n(z.B. falsche Zahlenart oder Komma statt Punkt bei Kommazahlen)")        
         
         
-        if eingabe == ergebnis:
-            msgbox("Sehr gut, das war richtig!")
+        if eingabe == None:
+            break
         
         else:
+            if round(eingabe_korrigiert,2) == round(ergebnis,2):
+                msgbox("Sehr gut, das war richtig!")
+                i += 2
             
-            if i == 0:
-                msgbox("Schade, das war falsch. Probiere es noch einmal.\nWenn du es dieses mal richtig machst, gibt es einen halben Punkt.")
-                richtige_loesungen -= 0.5
-                
             else:
-                msgbox("Schade, das war falsch.\nDas gibt leider keine Punkte.")
-                richtige_loesungen -= 0.5    
+                
+                if i == 0:
+                    msgbox("Schade, das war falsch. Probiere es noch einmal.\nWenn du es dieses mal richtig machst, gibt es einen halben Punkt.")
+                    richtige_loesungen -= 0.5
+                    i += 1
+
+                else:
+                    msgbox("Schade, das war falsch.\nDas gibt leider keine Punkte.")
+                    richtige_loesungen -= 0.5
+                    i += 1
+
+    if eingabe == None:
+        startseite()
+    else:
+        aufgabenliste.append(aufgabenloesung)
+        nutzer_loesungsliste.append(eingabe_korrigiert)
 #----------------------------------------------------------------------------------------------------
 
 
@@ -269,4 +360,8 @@ def zahlenraum():
 #----------------------------------------------------------------------------------------------------
 
 
-Addition(1,10)
+Division(1,10)
+
+
+print(aufgabenliste)
+print(nutzer_loesungsliste)
