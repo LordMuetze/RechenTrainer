08552dc8098c0e913f22a37a6f3d3add4cee17f9
LordMuetze <valentin-herrmann@gmx.net>
2017-12-30 20:26:46 +0100

    zahlenraum() gekuerzt

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ anzahl_aufgaben = 0
 
 
 
-
+#--------------------Struktogramm fertig--------------------
 #--------------------Definition der Startseite--------------------
 def startseite():
 
@@ -42,18 +42,14 @@ def startseite():
     global aufgabenliste
     global nutzer_loesungsliste
     global rechenart
-    global min_zahl
+    global min_zahl1
     global max_zahl1
     global min_zahl2
     global max_zahl2
     global anzahl_aufgaben
 
 
-    choices = ["Los gehts", "Verlassen"] #Festlegen der Buttons
-    start = buttonbox("Herzlich Willkommen beim Rechentrainer", "Willkommen", choices, "zahlenbild.gif") #angeklickten Button an Variable binden
-
-
-    #Reset der wichtigsten globalen Variablen mit jeder neuen Runde
+    #Reset der wichtigsten globalen Variablen & Listen mit jedem neuen Durchlauf des Rechen Trainers
     richtige_loesungen = 0.0
     aufgabenliste = []
     nutzer_loesungsliste = []
@@ -63,13 +59,18 @@ def startseite():
     min_zahl2 = None
     max_zahl2 = None
     anzahl_aufgaben = 0
-    
+
+
+    choices = ["Los gehts", "Verlassen"] #Festlegen der Buttons
+    start = buttonbox("Herzlich Willkommen beim Rechentrainer", "Willkommen", choices, "zahlenbild.gif") #angeklickten Button an Variable binden
+
     return start #angeklickten Button als Funktionswert ausgeben
 #----------------------------------------------------------------------------------------------------
 
 
 
 
+#--------------------Struktogramm fertig--------------------
 #--------------------Definition, welche Rechenart trainiert werden soll--------------------
 def rechenart_eingeben():
 
@@ -77,14 +78,16 @@ def rechenart_eingeben():
 
     while rechenart == []: #Schleife läuft, solange keine Rechenart ausgewählt wurde
 
-        choices = ["Addition","Subtraktion","Multiplikation","Division"] #wählbare Rechenarten festlegen
+        choices = ["Addition","Subtraktion","Multiplikation","Division","Potenzierung (Achtung: zweite Zahl ist der Exponent, sehr hohe Werte vermeiden!)"] #wählbare Rechenarten festlegen
         rechenart = multchoicebox("Welche Rechenarten willst du trainieren?","Rechenart",choices) #Liste aus ausgewählte(n) Rechenart(en) erstellen
-    return rechenart #ausgewählte Rechenarten als Funktionswert zurückgeben
+    
+    return rechenart #ausgewählte Rechenarten [Liste] als Funktionswert zurückgeben
 #----------------------------------------------------------------------------------------------------
 
 
 
 
+#--------------------Struktogramm fertig--------------------
 #--------------------Definition, welche Rechenart trainiert werden soll--------------------
 def rechenart_abrufen():
 
@@ -104,6 +107,9 @@ def rechenart_abrufen():
 
     elif rechentyp == "Division":
         eingabe = Rechnung("/")
+    
+    elif rechentyp == "Potenzierung (Achtung: zweite Zahl ist der Exponent, sehr hohe Werte vermeiden!)":
+        eingabe = Rechnung("**")
 
     #wird keine möglichen Rechenart ausgewählt, wird ein SystemError ausgegeben und "eingabe" als NoneType gesetzt
     else:
@@ -116,6 +122,67 @@ def rechenart_abrufen():
 
 
 
+#--------------------Festlegung des Zahlenraums--------------------
+def zahlenraum():
+
+    global min_zahl1
+    global max_zahl1
+    global min_zahl2
+    global max_zahl2
+
+    for i in range (1,3): #for-Schleife mit zwei Durchläufen: i=1 & i=2
+
+        while True: #Endlosschleife
+
+            try: #try-Teil wird ausgeführt, bis ein Fehler auftritt
+                fields = ["min. Zahl","max. Zahl"] #Namen der Felder festlegen
+                values = [1,100] #Standardwerte für die Felder festlegen
+
+                zahlenraum = multenterbox("In welchem Bereich soll die erste Zahl liegen?","Zahlenraum",fields,values)
+
+
+                if zahlenraum == None: #Cancel-Button wurde gedrückt
+                    break #Endlosschleife abbrechen
+
+                else:
+                    max_zahl = int(zahlenraum.pop()) #letzten Wert der Liste als Maximalwert festlegen und von der Liste entfernen
+                    min_zahl = int(zahlenraum.pop()) #letzten Wert der Liste als Minimalwert festlegen und von der Liste entfernen
+                    #wird ein nicht-Integer (Float,Text,...) eingegeben, wird ein Fehler auftreten
+
+
+                    #prüfen, ob der Maximalwert größer als der Minimalwert ist
+                    #oder ob eine der beiden Zahlen kleiner als 0 ist (keine natürliche Zahl)
+                    if max_zahl < min_zahl or max_zahl < 0 or min_zahl < 0:
+                        msgbox("Sorry, da war was bei deiner Eingabe falsch")
+                    
+                    else: #Zahlen sind ok
+                        break #Endlosschleife abbrechen
+
+            except: #tritt ein Fehler auf, so wird der except-Teil ausgeführt
+                msgbox("Sorry, da war was bei deiner Eingabe falsch")
+        
+        if zahlenraum == None: #Cancel-Button wurde gedrückt
+            break #for-Schleife abbrechen
+        else:
+            pass #nichts tun
+
+        #Strings zum späteren Ausführen erstellen, Nummer von min_zahl & max_zahl abhängig von Schleifendurchlauf
+        # global min_zahl1
+        # min_zahl1 = min_zahl
+        string_min_zahl = "global min_zahl" + str(i) + "; min_zahl" + str(i) + " = min_zahl"
+        string_max_zahl = "global max_zahl" + str(i) + "; max_zahl" + str(i) + " = max_zahl"
+
+        #min_zahl & max_zahl, min_zahl1 & max_zahl1 bzw min_zahl2 & max_zahl2 zuweisen
+        exec(string_min_zahl)
+        exec(string_max_zahl)
+
+    return zahlenraum #die Liste "zahlenraum" wird als Funktionswert ausgegeben
+#----------------------------------------------------------------------------------------------------
+
+
+
+
+#--------------------Struktogramm fertig--------------------
 #--------------------Anzahl der Aufgaben--------------------
 def anzahl_aufgaben_eingeben():
 
@@ -130,7 +197,8 @@ def anzahl_aufgaben_eingeben():
 
 
 
-#--------------------Definition der einzelnen Aufgabenstellung, rechenart-spezifisch, inhaltlich gleich--------------------
+#--------------------Struktogramm fertig--------------------
+#--------------------Aufgabenstellung mit Eingabe für die Lösung--------------------
 def Rechnung(operator):
 
     global richtige_loesungen
@@ -195,7 +263,7 @@ def Rechnung(operator):
                     i += 1 #Zähler der Schleife um 1 (auf 2) erhöhen
 
     if eingabe == None: #Cancel-Button wurde gdedrückt
-        pass
+        pass #nichts tun
     else:
         aufgabenliste.append(aufgabenloesung) #Aufgabe mit Lösung zur Liste für die Übersicht hinzufügen
         nutzer_loesungsliste.append(eingabe_korrigiert) #Lösung des Nutzers zur Liste für die Übersicht hinzufügen
@@ -206,89 +274,7 @@ def Rechnung(operator):
 
 
 
-#--------------------Festlegung des 1. Zahlenraums--------------------
-def zahlenraum1():
-
-    global min_zahl1
-    global max_zahl1
-
-
-    while True: #Endlosschleife
-
-        try: #try-Teil wird ausgeführt, bis ein Fehler auftritt
-            fields = ["min. Zahl","max. Zahl"] #Namen der Felder festlegen
-            values = [1,100] #Standardwerte für die Felder festlegen
-
-            zahlenraum_1 = multenterbox("In welchem Bereich soll die erste Zahl liegen?","Zahlenraum",fields,values)
-
-
-            if zahlenraum_1 == None: #Cancel-Button wurde gedrückt
-                break #Endlosschleife abbrechen
-
-            else:
-                max_zahl1 = int(zahlenraum_1.pop()) #letzten Wert der Liste als Maximalwert festlegen und von der Liste entfernen
-                min_zahl1 = int(zahlenraum_1.pop()) #letzten Wert der Liste als Minimalwert festlegen und von der Liste entfernen
-                #wird ein nicht-Integer (Float,Text,...) eingegeben, wird ein Fehler auftreten
-
-
-                #prüfen, ob der Maximalwert größer als der Minimalwert ist
-                #oder ob eine der beiden Zahlen kleiner als 0 ist (keine natürliche Zahl)
-                if max_zahl1 < min_zahl1 or max_zahl1 < 0 or min_zahl1 < 0:
-                    msgbox("Sorry, da war was bei deiner Eingabe falsch")
-                
-                else: #Zahlen sind ok
-                    break #Endlosschleife abbrechen
-
-        except: #tritt ein Fehler auf, so wird der except-Teil ausgeführt
-            msgbox("Sorry, da war was bei deiner Eingabe falsch")
-    
-    return zahlenraum_1 #die Liste "zahlenraum_1" wird als Funktionswert ausgegeben
-#----------------------------------------------------------------------------------------------------
-
-
-
-
-#--------------------Festlegung des 2. Zahlenraums--------------------
-def zahlenraum2():
-
-    global min_zahl2
-    global max_zahl2
-
-
-    while True: #Endlosschleife
-
-            try: #try-Teil wird ausgeführt, bis ein Fehler auftritt
-                fields = ["min. Zahl","max. Zahl"] #Namen der Felder festlegen
-                values = [1,100] #Standardwerte für die Felder festlegen
-
-                zahlenraum_2 = multenterbox("In welchem Bereich soll die zweite Zahl liegen?","Zahlenraum",fields,values)
-
-
-                if zahlenraum_2 == None: #Cancel-Button wurde gedrückt
-                    break #Endlosschleife abbrechen
-
-                else:
-                    max_zahl2 = int(zahlenraum_2.pop()) #letzten Wert der Liste als Maximalwert festlegen und von der Liste entfernen
-                    min_zahl2 = int(zahlenraum_2.pop()) #letzten Wert der Liste als Minimalwert festlegen und von der Liste entfernen
-                    #wird ein nicht-Integer (Float,Text,...) eingegeben, wird ein Fehler auftreten
-
-                    #prüfen, ob der Maximalwert größer als der Minimalwert ist
-                    #oder ob eine der beiden Zahlen kleiner als 0 ist (keine natürliche Zahl)
-                    if max_zahl2 < min_zahl2 or max_zahl2 < 0 or min_zahl2 < 0:
-                        msgbox("Sorry, da war was bei deiner Eingabe falsch")
-                        
-                    else: #Zahlen sind ok
-                        break #Endlosschleife abbrechen
-
-            except: #tritt ein Fehler auf, so wird der except-Teil ausgeführt
-                msgbox("Sorry, da war was bei deiner Eingabe falsch")
-
-    return zahlenraum_2 #die Liste "zahlenraum_2" wird als Funktionswert ausgegeben
-#----------------------------------------------------------------------------------------------------
-
-
-
-
+#--------------------Struktogramm fertig--------------------
 #--------------------erstellen der Abschlussuebersicht--------------------
 def uebersicht():
 
@@ -313,7 +299,7 @@ def uebersicht():
     Dateiname = filesavebox(filetypes=[".txt"]) #Speicherort und Dateiname festlegen
 
     if Dateiname == None: #Cancel-Button gedrückt
-        pass #nicht tun & nicht speichern
+        pass #nichts tun & nicht speichern
 
     else:
         datei = open(Dateiname,"w") #Datei mit obigem Namen erstellen/öffnen und leeren
@@ -326,6 +312,7 @@ def uebersicht():
 
 
 
+#--------------------Struktogramm fertig--------------------
 #--------------------Erstellen der main-Funktion--------------------
 #--------------------Gesamtablauf und Zusammenfuehren vorheriger Funktionen--------------------
 def main():
@@ -341,28 +328,21 @@ def main():
             if rechenart_testen == None: #Cancel-Button wurde in dieser Funktion gedrückt
                 break #Endlosschleife und damit weiteren Programmablauf abbrechen 
             else:
-                pass
+                pass #nichts tun
             
             
-            zahlenraum1_testen = zahlenraum1()
-            if zahlenraum1_testen == None: #Cancel-Button wurde in dieser Funktion gedrückt
+            zahlenraum_testen = zahlenraum()
+            if zahlenraum_testen == None: #Cancel-Button wurde in dieser Funktion gedrückt
                 break #Endlosschleife und damit weiteren Programmablauf abbrechen
             else:
-                pass
-            
-
-            zahlenraum2_testen = zahlenraum2()
-            if zahlenraum2_testen == None: #Cancel-Button wurde in dieser Funktion gedrückt
-                break #Endlosschleife und damit weiteren Programmablauf abbrechen
-            else:
-                pass
-            
+                pass #nichts tun
+                        
             
             anzahl_aufgaben_testen = anzahl_aufgaben_eingeben()
             if anzahl_aufgaben_testen == None: #Cancel-Button wurde in dieser Funktion gedrückt
                 break #Endlosschleife und damit weiteren Programmablauf abbrechen
             else:
-                pass
+                pass #nichts tun
 
 
             for i in range(0,anzahl_aufgaben): #untere Funktion sooft aufrufen, wie Aufgaben gerechnet werden sollen
@@ -370,14 +350,14 @@ def main():
                 if rechenart_abrufen_testen == None: #Cancel-Button wurde in dieser Funktion gedrückt
                     break #Endlosschleife und damit weiteren Programmablauf abbrechen
                 else:
-                    pass
+                    pass #nichts tun
 
             try: #try-Teil wird solange ausgeführt bis ein Fehler auftritt
                 uebersicht_testen = uebersicht()
                 if uebersicht_testen == None: #Cancel-Button wurde in dieser Funktion gedrückt
                     break #Endlosschleife und damit weiteren Programmablauf abbrechen
                 else:
-                    pass
+                    pass #nichts tun
             except: #tritt ein Fehler auf, wird der except-Teil ausgeführt
                 break #Endlosschleife und damit weiteren Programmablauf abbrechen
 
